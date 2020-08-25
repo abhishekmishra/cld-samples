2d7ee041ab9a8b2c50e55b8ec73c37ebe5e0a726
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-08-25 12:32:59 +0530

    added a test script to test availability

---
+++ testlibs.lua
@@ -0,0 +1,30 @@
+json = require("json")
+docker = require("luaclibdocker")
+CLD = require("cld")
+
+d = docker.connect()
+d:set_loglevel(2)
+local cld = CLD:new(d)
+
+print("---------------------------------------------------")
+print("Container list as JSON from clibdocker")
+-- use clibdocker to get json output for containers list
+local ctr_ls_str = d:container_ls_filter(false, 0, false, json.encode(nil))
+local ctr_ls = json.decode(ctr_ls_str)
+print(ctr_ls_str)
+
+-- use CLD library to list containers
+print("---------------------------------------------------")
+print("container list from CLD")
+ls_options = {
+    ["all"] = {["val"] = false},
+    ["filter"] = {["val"] = nil},
+    ["format"] = {["val"] = nil},
+    ["last"] = {["val"] = 0},
+    ["latest"] = {["val"] = false},
+    ["no-trunc"] = {["val"] = false},
+    ["quiet"] = {["val"] = false},
+    ["size"] = {["val"] = false}
+}
+output = cld:run("container", "ls", ls_options)
+-- print(output)
